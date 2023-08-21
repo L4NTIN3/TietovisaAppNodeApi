15dc361bfb627c802b9abd68b70315dc86e0bae3
Tommi Virtanen <tommiv01@outlook.com>
2023-08-21 23:43:09 +0300

    api done

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require("express")
 const app = express();
 const fs = require("fs")
+const cors = require("cors")
 const port = 3000;
 
 app.use(express.json())
 const databasePath = "questions.json"
 
-app.post("/question", (req, res) => {
+const allowedOrigins = ['https://ashy-sky-0b6965303.3.azurestaticapps.net', "http://localhost:5173"]
+const corsOptions = {
+    origin: allowedOrigins, // Allow requests from this domain
+    methods: 'GET,PUT,POST,DELETE',    // Specify allowed HTTP methods
+    optionsSuccessStatus: 204 
+}
+
+app.use(cors(corsOptions))
+
+
+
+const allowedApiKey = 'fzCXTefLOAL83YfNr0lrFhNk67FlQByygjOgugk45HzJIuxBWjcOeNmyfu5Zy74dtedIrTc0KsEkfBtXc0KsEkfBtXf9eotEc8Lz';
+
+// Middleware tarkistamaan API-avain
+function checkApiKey(req, res, next) {
+  const apiKey = req.headers['api-key'];
+
+  if (apiKey === allowedApiKey) {
+    next(); // Jatka pyyntöjen käsittelyyn
+  } else {
+    res.status(403).json({ message: 'Access denied. Invalid API key.' });
+  }
+}
+
+app.post("/question", checkApiKey, (req, res) => {
 
     const data = req.body; 
     const existingData = JSON.parse(fs.readFileSync(databasePath, "utf8"));
@@ -18,7 +43,7 @@ app.post("/question", (req, res) => {
 })
 
 
-app.get("/questions", (req, res) => {
+app.get("/questions", checkApiKey, (req, res) => {
     const questions = fs.readFileSync(databasePath, "utf8")
     res.send(questions)
 })
