61b0222831c47c8910c813a3e97eed54c565f23f
Tommi Virtanen <tommiv01@outlook.com>
2023-08-22 00:07:00 +0300

    api done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const fs = require("fs")
 const cors = require("cors")
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 const databasePath = "questions.json"
